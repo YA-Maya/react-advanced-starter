7813c2e6a76051468e3061ac93ca98f4869ef607
Ya-Maya <Sumeyask6@gmail.com>
2023-04-19 10:21:54 +0100

    push my code so that i can use it on my other laptop

--- app/src/01-useState/useState-object.jsx
+++ app/src/01-useState/useState-object.jsx
@@ -1,7 +1,28 @@
 import { useState } from "react";
 
-const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+const useStateObject = () => {
+  const [person, setPerson] = useState({
+    name:'Sumeya',
+    age: 21,
+    hobby:'playing chess'
+  })  ;
+
+  const handleClick=()=>{
+    setPerson({ ...person, name:'Peter'});
+    
+  };
+
+  return(
+    
+  <>
+  <div>
+    <h1>{person.name}</h1>
+    <h2>{person.age}</h2>
+    <h2>Enjoys {person.hobby}</h2>
+    <button className="bt" onClick={handleClick} type="button">Change Me</button>
+    </div>
+  </>
+  )
 };
 
-export default UseStateObject;
+export default useStateObject;
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 // import ErrorExample from './01-useState/error-example'
 // import UseStateBasics from './01-useState/useState-basics'
-import UseStateArray from './01-useState/useState-array'
+// import UseStateArray from './01-useState/useState-array'
+// import UseStateObject from "./01-useState/useState-object";
+
+import UseEffectBasics from './02-useEffect/useEffect-basics';
+
 
 function App() {
   return (
     <div className="container">
-     <UseStateArray/>
+      <UseEffectBasics />
     </div>
   )
 }
 
-export default App
+export default App;
+
